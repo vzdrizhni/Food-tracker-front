3dfc13076f1796a214c3dc4f3505794d6efee5a9
vzdrizhni <vzdrizhni@gmail.com>
2020-09-27 13:56:12 +0500

    Updates

---
+++ precache-manifest.b7fce071f0e79275e9487b7289389d84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd0af5360e036bb008b5eb49af2a8d5",
+    "url": "/vzdrizhni/Food-tracker-front/index.html"
+  },
+  {
+    "revision": "45c08cc96ad9b7dcebed",
+    "url": "/vzdrizhni/Food-tracker-front/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "326822ca311c98a885e9",
+    "url": "/vzdrizhni/Food-tracker-front/static/css/main.b6187bd2.chunk.css"
+  },
+  {
+    "revision": "45c08cc96ad9b7dcebed",
+    "url": "/vzdrizhni/Food-tracker-front/static/js/2.a25257bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vzdrizhni/Food-tracker-front/static/js/2.a25257bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326822ca311c98a885e9",
+    "url": "/vzdrizhni/Food-tracker-front/static/js/main.28605a27.chunk.js"
+  },
+  {
+    "revision": "9ca11d6d30d978f09cfe",
+    "url": "/vzdrizhni/Food-tracker-front/static/js/runtime-main.dd6eee74.js"
+  },
+  {
+    "revision": "e6a0817ad8615ea50c139cf311c27db5",
+    "url": "/vzdrizhni/Food-tracker-front/static/media/body-scale.e6a0817a.svg"
+  },
+  {
+    "revision": "92bd9476817f0f74c82c672efd870ee9",
+    "url": "/vzdrizhni/Food-tracker-front/static/media/meal.92bd9476.svg"
+  }
+]);
